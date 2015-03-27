34b19a6779efa58ed026dbb056988c61f838003c
Marco Mornati <ilmorna@gmail.com>
2015-03-27 17:30:53 +0100

    FEAT: Added dynamic proxy configuration
    If proxy is set on the docker container,mock will start the build using it (need to change the mock configuration file). If no proxy is configured, the mock build start in the normal way.

--- build-rpm.sh
+++ build-rpm.sh
@@ -7,38 +7,52 @@ OUTPUT_FOLDER=$MOUNT_POINT/output
 
 if [ -z "$MOCK_CONFIG" ]; then
         echo "MOCK_CONFIG is empty. Should bin one of: "
-	ls -l $MOCK_CONF_FOLDER
+        ls -l $MOCK_CONF_FOLDER
 fi
 if [ -z "$SOURCE_RPM" ] && [ -z "$SPEC_FILE" ]; then
         echo "You need to provide the src.rpm or spec file to build"
         echo "Set SOURCE_RPM or SPEC_FILE environment variables"
-	exit 1
+        exit 1
+fi
+
+#If proxy env variable is set, add the proxy value to the configuration file
+if [ ! -z "$HTTP_PROXY" ] || [ ! -z "$http_proxy" ]; then
+        TEMP_PROXY=""
+        if [ ! -z "$HTTP_PROXY" ]; then
+                TEMP_PROXY=$(echo $HTTP_PROXY | sed s/\\//\\\\\\//g)
+        fi
+        if [ ! -z "$http_proxy" ]; then
+                TEMP_PROXY=$(echo $http_proxy | sed s/\\//\\\\\\//g)
+        fi
+
+        echo "Configuring http proxy to the mock build file to: $TEMP_PROXY"
+        cp /etc/mock/$MOCK_CONFIG.cfg /tmp/$MOCK_CONFIG.cfg
+        sed s/\\[main\\]/\[main\]\\\nproxy=$TEMP_PROXY/g /tmp/$MOCK_CONFIG.cfg > /etc/mock/$MOCK_CONFIG.cfg
 fi
 
 if [ ! -d "$OUTPUT_FOLDER" ]; then
-	mkdir -p $OUTPUT_FOLDER
+        mkdir -p $OUTPUT_FOLDER
 else
-	rm -f $OUTPUT_FOLDER/*
+        rm -f $OUTPUT_FOLDER/*
 fi
 echo "=> Building parameters:"
 echo "========================================================================"
 echo "      MOCK_CONFIG:    $MOCK_CONFIG"
 #Priority to SOURCE_RPM if both source and spec file env variable are set
 if [ ! -z "$SOURCE_RPM" ]; then
-	echo "      SOURCE_RPM:     $SOURCE_RPM"
-	echo "========================================================================"
-	$MOCK_BIN -r $MOCK_CONFIG --rebuild $MOUNT_POINT/$SOURCE_RPM --resultdir=$OUTPUT_FOLDER
+        echo "      SOURCE_RPM:     $SOURCE_RPM"
+        echo "========================================================================"
+        $MOCK_BIN -r $MOCK_CONFIG --rebuild $MOUNT_POINT/$SOURCE_RPM --resultdir=$OUTPUT_FOLDER
 elif [ ! -z "$SPEC_FILE" ]; then
-	if [ -z "$SOURCES" ]; then
-		echo "You need to specify SOURCES env variable pointing to folder or sources file (only when building with SPEC_FILE)"
-		exit 1;
-	fi
-	echo "      SPEC_FILE:     $SPEC_FILE"
-	echo "      SOURCES:       $SOURCES"
-	echo "========================================================================"
- 	$MOCK_BIN -r $MOCK_CONFIG --buildsrpm --spec=$MOUNT_POINT/$SPEC_FILE --sources=$MOUNT_POINT/$SOURCES --resultdir=$OUTPUT_FOLDER 
-	$MOCK_BIN -r $MOCK_CONFIG --rebuild $(find $OUTPUT_FOLDER -type f -name "*.src.rpm") --resultdir=$OUTPUT_FOLDER
+        if [ -z "$SOURCES" ]; then
+                echo "You need to specify SOURCES env variable pointing to folder or sources file (only when building with SPEC_FILE)"
+                exit 1;
+        fi
+        echo "      SPEC_FILE:     $SPEC_FILE"
+        echo "      SOURCES:       $SOURCES"
+        echo "========================================================================"
+        $MOCK_BIN -r $MOCK_CONFIG --buildsrpm --spec=$MOUNT_POINT/$SPEC_FILE --sources=$MOUNT_POINT/$SOURCES --resultdir=$OUTPUT_FOLDER
+        $MOCK_BIN -r $MOCK_CONFIG --rebuild $(find $OUTPUT_FOLDER -type f -name "*.src.rpm") --resultdir=$OUTPUT_FOLDER
 fi
 
 echo "Build finished. Check results inside the mounted volume folder."
-
