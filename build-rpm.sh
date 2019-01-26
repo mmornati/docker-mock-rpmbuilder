0de7ec30416c020cbb90fa01a78cac11fe0c920e
Marco Mornati <marco.mornati@decathlon.com>
2019-01-26 17:36:27 +0100

    Changed empty variable condition

--- build-rpm.sh
+++ build-rpm.sh
@@ -51,7 +51,7 @@ if [ ! -z "$HTTP_PROXY" ] || [ ! -z "$http_proxy" ]; then
         sed s/\\[main\\]/\[main\]\\\nproxy=$TEMP_PROXY/g /tmp/$MOCK_CONFIG.cfg > /etc/mock/$MOCK_CONFIG.cfg
 fi
 
-if [ ! -z "$BUILD_ON_GITHUB" ]; then
+if [ -z "$BUILD_ON_GITHUB" ]; then
         OUTPUT_FOLDER=${OUTPUT_FOLDER}/${MOCK_CONFIG}
         if [ ! -d "$OUTPUT_FOLDER" ]; then
                 mkdir -p $OUTPUT_FOLDER
